5ef16639b73ad80525a201ce90cdb74289fa141b
Caleb Smith <CRSmith4@student.fullsail.edu>
2019-03-19 19:34:21 -0400

    Added a new screen for larger devices, and fixed crash issue when user doesn't add any values

--- app/src/main/java/com/example/applook/SecondScreen.java
+++ app/src/main/java/com/example/applook/SecondScreen.java
@@ -32,52 +32,78 @@ public class SecondScreen extends AppCompatActivity {
         String View = "Calories:";
         String View2 = " ";
         String View1 = " ";
-        EditText Weight = (EditText)findViewById(R.id.Weight);
-        EditText Height = (EditText)findViewById(R.id.Height);
-        EditText Age = (EditText)findViewById(R.id.Age);
-        TextView CalorieInfo = (TextView)findViewById(R.id.Result);
-        TextView Carbs = (TextView)findViewById(R.id.Result2);
-        TextView Fats = (TextView)findViewById(R.id.Result3);
-        TextView Viewer = (TextView)findViewById(R.id.Identiefier1);
-        TextView Viewer2 = (TextView)findViewById(R.id.Identeifier2);
-        TextView Viewer3 = (TextView)findViewById(R.id.Identifier3);
-        int WeightToInt =  Integer.parseInt(Weight.getText().toString());
-        int HeighttoInt = Integer.parseInt(Height.getText().toString());
-        int AgeInt = Integer.parseInt(Age.getText().toString());
-        double CalInfo =  66 + (6.23 *WeightToInt) + (12.7 * HeighttoInt) - (6.8 * AgeInt);
-        CalorieInfo.setText(Double.toString(CalInfo));
-        Carbs.setText(View1);
-        Fats.setText(View1);
-        Viewer.setText(View);
-        Viewer2.setText(View2);
-        Viewer3.setText(View1);
-        Carbs.setVisibility(Carbs.INVISIBLE);
-        Fats.setVisibility(Fats.INVISIBLE);
+        EditText Weight = (EditText) findViewById(R.id.Weight);
+        EditText Height = (EditText) findViewById(R.id.Height);
+        EditText Height2  = (EditText) findViewById(R.id.Height2);
+        EditText Age = (EditText) findViewById(R.id.Age);
+        TextView CalorieInfo = (TextView) findViewById(R.id.Result);
+        TextView Carbs = (TextView) findViewById(R.id.Result2);
+        TextView Fats = (TextView) findViewById(R.id.Result3);
+        TextView Viewer = (TextView) findViewById(R.id.Identiefier1);
+        TextView Viewer2 = (TextView) findViewById(R.id.Identeifier2);
+        TextView Viewer3 = (TextView) findViewById(R.id.Identifier3);
+
+        if (Weight.getText()!=null && Height.getText()!=null && Age.getText()!=null) {
+            int WeightToInt = Integer.parseInt(Weight.getText().toString());
+            int HeightToInt = Integer.parseInt(Height.getText().toString());
+           int Height2Tointt = Integer.parseInt(Height2.getText().toString());
+            int AgeInt = Integer.parseInt(Age.getText().toString());
+            double CalInfo = 66 + (6.23 * WeightToInt) + (12.7 * ((HeightToInt * 12) + Height2Tointt)) - (6.8 * AgeInt);
+            CalorieInfo.setText(Double.toString(CalInfo));
+            Carbs.setText(View1);
+            Fats.setText(View1);
+            Viewer.setText(View);
+            Viewer2.setText(View2);
+            Viewer3.setText(View1);
+            Carbs.setVisibility(Carbs.INVISIBLE);
+            Fats.setVisibility(Fats.INVISIBLE);
+        }
+        else {
+            Weight.setText("1");
+            Height.setText("1");
+            Age.setText("1");
+
+        }
+
     }
 
     public void Calculate2 (View view){
-        String View = "Protein:";
-        String View1 = "Carbs:";
-        String View2 = "Fats:";
+        String View = "Protein(g):";
+        String View1 = "Carbs(g):";
+        String View2 = "Fats(g):";
+
         EditText Weight = (EditText)findViewById(R.id.Weight);
+        if (Weight.getText()!= null) {
         int WeighttoINt = Integer.parseInt(Weight.getText().toString());
-        TextView Protien = (TextView)findViewById(R.id.Result);
-        TextView Carbs = (TextView)findViewById(R.id.Result2);
-        TextView Fats = (TextView)findViewById(R.id.Result3);
-        TextView Viewer1 = (TextView)findViewById(R.id.Identiefier1);
-        TextView Viewer2 = (TextView)findViewById(R.id.Identeifier2);
-        TextView Viewer3 = (TextView)findViewById(R.id.Identifier3);
-        double Protienintake =  (WeighttoINt * 4) * .36;
-        double Fatintake = (WeighttoINt * 9) * .30;
-        double CarbIntake = (WeighttoINt * 4) *.45;
-        Protien.setText(Double.toString(Protienintake));
-        Carbs.setText((Double.toString(CarbIntake)));
-        Fats.setText(Double.toString(Fatintake));
-        Viewer1.setText(View);
-        Viewer2.setText(View2);
-        Viewer3.setText(View1);
-        Carbs.setVisibility(Carbs.VISIBLE);
-        Fats.setVisibility(Fats.VISIBLE);
+            TextView Protien = (TextView)findViewById(R.id.Result);
+            TextView Carbs = (TextView)findViewById(R.id.Result2);
+            TextView Fats = (TextView)findViewById(R.id.Result3);
+            TextView Viewer1 = (TextView)findViewById(R.id.Identiefier1);
+            TextView Viewer2 = (TextView)findViewById(R.id.Identeifier2);
+            TextView Viewer3 = (TextView)findViewById(R.id.Identifier3);
+            double Protienintake = (WeighttoINt * 4) * .36;
+            double Fatintake = (WeighttoINt * 9) * .30;
+            double CarbIntake = (WeighttoINt * 4) * .45;
+            Protien.setText(Double.toString(Protienintake));
+            Carbs.setText((Double.toString(CarbIntake)));
+            Fats.setText(Double.toString(Fatintake));
+            Viewer1.setText(View);
+            Viewer2.setText(View2);
+            Viewer3.setText(View1);
+            Carbs.setVisibility(Carbs.VISIBLE);
+            Fats.setVisibility(Fats.VISIBLE);
+        }
+        else{
+            TextView Protien = (TextView)findViewById(R.id.Result);
+            TextView Carbs = (TextView)findViewById(R.id.Result2);
+            TextView Fats = (TextView)findViewById(R.id.Result3);
+            Carbs.setVisibility(Carbs.VISIBLE);
+            Fats.setVisibility(Fats.VISIBLE);
+            Protien.setText("0");
+            Carbs.setText("0");
+            Fats.setText("0");
+
+        }
 
 
     }
